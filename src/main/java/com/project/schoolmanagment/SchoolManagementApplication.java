8ab515a641177612cbf82155ae1389fa6cb48b04
likeahoneybadger <aksoyyusufg@gmail.com>
2024-01-15 16:08:58 +0100

    First Version

---
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -0,0 +1,29 @@
+package com.project.schoolmanagment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+
+
+
+    public SchoolManagementApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementApplication.class, args);
+        //this is our first day
+        //this is our first git-push for back-end 
+        //application properties file has changed, lets see what will happen
+    }
+
+}
+
+
+
+
+
+
+
+
+
